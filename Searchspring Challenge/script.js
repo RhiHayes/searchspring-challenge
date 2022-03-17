58d76d2129d23d77402fedc014925248e0853654
Rhiannon Hayes <88731446+RhiHayes@users.noreply.github.com>
2022-03-16 19:00:42 -0500

    Fetching Data + Got Results on Page

--- Searchspring Challenge/script.js
+++ Searchspring Challenge/script.js
@@ -14,11 +14,48 @@ function userRequest(q) {
     const searchParameters = new URLSearchParams({
         resultsFormat: "native",
         siteId: "scmq7n",
-        q: q
+        q: q,
+        resultsPerPage: 21
     });
 
     const editedURL = searchSpringAPI + "?" + searchParameters;
 
-    console.log(editedURL);
+    fetch(editedURL) //Getting URL
+
+    .then(response => response.json())
+        .then(data => {
+            console.log(data.results) //Turns it into manipulable data
+            renderProducts(data.results);
+
+        })
+        .catch(error =>
+            console.log(error)) //Catches errors if they arise
+}
+
+
+
+function renderProducts(products) {
+
+    let html = ""; //Clears html out everytime function is called
+
+    products.forEach(result => {
+
+        let hasSale = result.hasOwnProperty("msrp");
+
+        let originalPrice = parseFloat(result.price).toFixed(2)
+        let salePrice = parseFloat(result.msrp).toFixed(2)
+
+        html += "<img src=\"" + result.thumbnailImageUrl + "\">" +
+        "<h1>" + result.name + "</h1>"
+
+        if (!hasSale || originalPrice >= salePrice) {
+            html += "<p>$" + originalPrice + "</p>"
+        } else {
+            html += "<p><strike>$" + salePrice + "</strike></p>" + " " +
+                "<p>$" + originalPrice + "</p>"
+        }
+    })
+
+    document.getElementById("results").innerHTML = html
 
 }
